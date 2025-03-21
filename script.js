dafe0f28c1959b23dd7af0e3dcc9f1296943c546
Tanu3012 <tanujafartyal01@gmail.com>
2025-03-21 16:01:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const todoInput = document.getElementById('todo-input'); 
+
+
+    const addTodoBtn = document.getElementById('addTodoBtn');
+    const todoList = document.getElementById('todoList');
+
+    addTodoBtn.addEventListener('click', addTodo);
+    todoInput.addEventListener('keypress', function (e){
+        if(e.key === 'Enter'){
+            addTodo();
+        }
+    });
+
+    function addTodo(){
+        const todoText = todoInput.value.trim();
+        if (todoText !== '') {
+            const li = document.createElement('li');
+            li.textContent = todoText;
+            todoList.appendChild(li);
+            todoInput.value='';
+        }
+    } 
+
+
